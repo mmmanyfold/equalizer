0489ae9e99463d7b50b5fb03e7815ceeac8c4619
eemshi <oreneemshi@gmail.com>
2018-02-27 11:37:13 -0700

    adds timepicker to A8

--- screens/onboarding/A8.js
+++ screens/onboarding/A8.js
@@ -10,32 +10,22 @@ import {
     TextInput,
 } from 'react-native';
 
-import DatePicker from 'react-native-datepicker'
+import DatePicker from 'react-native-datepicker';
+import TimePicker from 'react-native-simple-time-picker';
 import { NextButton, BackButton } from '../../components/OnboardNavButtons';
 import { MonoText } from '../../components/StyledText';
 
 export default class A8 extends React.Component {
+  state = {
+    selectedHours: 0,
+    selectedMinutes: 0,
+  }
   static navigationOptions = {
     header: null,
   };
 
-  state = {
-      childNickname: '',
-      childDOB: '2017-02-25',
-  };
-
-  handleNicknameInput(childNickname) {
-     this.setState({ childNickname });
-  }
-
-  saveAction() {
-      const { navigation : { state : { params: { store } } } } = this.props;
-      const update = store.set('childNickname', this.state.childNickname);
-      const update2 = update.set('childDOB', this.state.childDOB);
-      return Promise.resolve(update2);
-  }
-
   render() {
+      const { selectedHours, selectedMinutes } = this.state;
       return (
           <View style={styles.container}>
               <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
@@ -47,47 +37,22 @@ export default class A8 extends React.Component {
                           style={styles.welcomeImage}
                       />
                   </View>
-                  {/* TODO: Add clock */}
+
                   <View style={styles.getStartedContainer}>
-                      <MonoText>Please enter the following information about your child</MonoText>
+                      <Text style={styles.getStartedText}>At the same time each day, we’ll send a notification reminding you to choose an Action.</Text>
+                      <Text style={styles.tabBarInfoText}>Pick a time after work when you are likely to be at home. (You can always change this later in your Settings.)</Text>
                   </View>
 
                   <View style={styles.helpContainer}>
-
-                      <Text style={styles.getStartedText}>{"Child's name (or nickname):"}</Text>
-
-                      <TextInput
-                          style={{width: 250, height: 40, borderColor: 'gray', borderWidth: 1}}
-                          onChangeText={this.handleNicknameInput.bind(this)}
-                          value={this.state.userFirstName} />
-
-                      <Text style={styles.getStartedText}>{"Child's Date of Birth:"}</Text>
-
-                      <DatePicker
-                          style={{width: 200}}
-                          date={this.state.childDOB}
-                          mode="date"
-                          placeholder="select date"
-                          format="YYYY-MM-DD"
-                          confirmBtnText="Confirm"
-                          cancelBtnText="Cancel"
-                          customStyles={{
-                              dateIcon: {
-                                  position: 'absolute',
-                                  left: 0,
-                                  top: 4,
-                                  marginLeft: 0
-                              },
-                              dateInput: {
-                                  marginLeft: 36
-                              },
-                          }}
-                          onDateChange={(childDOB) => this.setState({ childDOB })}
+                      <TimePicker
+                        selectedHours={selectedHours}
+                        selectedMinutes={selectedMinutes}
+                        onChange={(hours, minutes) => this.setState({ selectedHours: hours, selectedMinutes: minutes })}
                       />
 
                       <Button
                         onPress={() => this.props.navigation.navigate('Main App Demo')}
-                        title="Take me to the app!"
+                        title="Finish Setup!"
                         color="#6FCF97"
                       />
                       <BackButton navigation={this.props.navigation}/>
@@ -102,13 +67,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-    },
-    developmentModeText: {
-        marginBottom: 20,
-        color: 'rgba(0,0,0,0.4)',
-        fontSize: 14,
-        lineHeight: 19,
-        textAlign: 'center',
     },
     contentContainer: {
         paddingTop: 30,
@@ -129,17 +87,6 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         marginHorizontal: 50,
     },
-    homeScreenFilename: {
-        marginVertical: 7,
-    },
-    codeHighlightText: {
-        color: 'rgba(96,100,109, 0.8)',
-    },
-    codeHighlightContainer: {
-        backgroundColor: 'rgba(0,0,0,0.05)',
-        borderRadius: 3,
-        paddingHorizontal: 4,
-    },
     getStartedText: {
         fontSize: 17,
         color: 'rgba(96,100,109, 1)',
@@ -147,43 +94,9 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight:'bold',
     },
-    tabBarInfoContainer: {
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        right: 0,
-        ...Platform.select({
-            ios: {
-                shadowColor: 'black',
-                shadowOffset: { height: -3 },
-                shadowOpacity: 0.1,
-                shadowRadius: 3,
-            },
-            android: {
-                elevation: 20,
-            },
-        }),
-        alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
     tabBarInfoText: {
         fontSize: 17,
         color: 'rgba(96,100,109, 1)',
         textAlign: 'center',
-    },
-    navigationFilename: {
-        marginTop: 5,
-    },
-    helpContainer: {
-        marginTop: 15,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
     },
 });
